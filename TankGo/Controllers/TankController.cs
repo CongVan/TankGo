4b3ffdb5535391ccc6624d27d22a684b934ca197
VanHC <VanHC@DESKTOP-6DK60H1>
2018-04-07 10:20:14 +0700

    Add project files.

---
+++ TankGo/Controllers/TankController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TankGo.Controllers
+{
+    public class TankController : Controller
+    {
+        // GET: Tank
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult LoadMap(string mapid)
+        {
+            string[] lines = System.IO.File.ReadAllLines(HttpContext.Server.MapPath("/layouts/" + mapid + ".txt"));
+
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+            
+            return Json(lines.ToList(),JsonRequestBehavior.AllowGet);
+        }
+    }
+}
