cf39a37ad05bf5ff3f400ed7c6fd7352e2b438bd
VanHC <VanHC@DESKTOP-6DK60H1>
2018-04-11 20:22:42 +0700

    add generator matrix change image wall

--- TankGo/Controllers/TankController.cs
+++ TankGo/Controllers/TankController.cs
@@ -9,6 +9,8 @@ namespace TankGo.Controllers
 {
     public class TankController : Controller
     {
+        List<Cell> Map = new List<Cell>();
+        List<List<int>> Matrix = new List<List<int>>();
         // GET: Tank
         public ActionResult Index()
         {
@@ -20,7 +22,7 @@ namespace TankGo.Controllers
             string[] lines = System.IO.File.ReadAllLines(HttpContext.Server.MapPath("/layouts/" + mapid + ".txt"));
             int rows=0, cols=0;
             cols = lines[0].Length;
-            var Map = new List<Cell>();
+            
             foreach (string line in lines)
             {
                 
@@ -43,8 +45,44 @@ namespace TankGo.Controllers
                 }
                 rows++;
             }
-            
+            GeneratorMatrixFromMaze();
             return Json(new {Map=Map,Rows=rows,Cols=cols },JsonRequestBehavior.AllowGet);
+        }
+
+        public void GeneratorMatrixFromMaze()
+        {
+            for (int i = 0; i < Map.Count; i++)
+            {
+                List<int> Temp = new List<int>();
+                for (int j = 0; j < Map.Count; j++)
+                {
+                    Temp.Add(-1);    // Tại điểm = -1 => Ko thể đi tới	
+                }
+                Matrix.Add(Temp);
+            }
+
+            for (int i = 0; i < Map.Count; i++)
+            {
+                for (int j = 0; j < Map.Count; j++)
+                {
+                    //Tọa độ cố định
+                    int x1 = Map[i].Col; // mygraph.GetArrDinhN(i).GetX();
+                    int y1 = Map[i].Row; // mygraph.GetArrDinhN(i).GetY();
+
+                    //Lấy tọa độ điểm cần tới
+                    int x2 = Map[j].Col; // mygraph.GetArrDinhN(j).GetX();
+                    int y2 = Map[j].Row; // mygraph.GetArrDinhN(j).GetY();
+
+                    if ((x1 + 1 == x2 && y1 == y2) || (x1 - 1 == x2 && y1 == y2) ||
+                        (x1 == x2 && y1 + 1 == y2) || (x1 == x2 && y1 - 1 == y2))
+                    {
+                        
+                        Matrix[i][j] = 1;
+                    }
+                }
+            }
+
+
         }
     }
 }
